2c4b87050033d0c8685cd54774e7b8a200122233
Alex Hauser <hauserap@s.dcsdk12.org>
2019-08-21 12:35:09 -0600

    add lincoln

--- Git.java
+++ Git.java
@@ -1,13 +1,14 @@
 public class Git{
 
-public static void main(String[] args){
+    public static void main(String[] args){
 
 
 
-    System.out.println("\"git is fun!!!!!!!!!!!!!\"");
-    System.out.println("hi Mom!");
-    System.out.println("tayler kind of cute!!!");
-    System.out.println("Noah is a baddy");
+     System.out.println("\"git is fun!!!!!!!!!!!!!\"");
+     System.out.println("hi Mom!");
+     System.out.println("tayler kind of cute!!!");
+     System.out.println("Noah is a baddy");
+     System.out.println("lincoln is a war criminal");
 }
 
 
